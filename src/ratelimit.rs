86fddc3d5a6f3aed13b324983a554a939a2ce701
ark0f <arsenylyashenko.3@gmail.com>
2020-03-31 18:09:35 +0300

    Get rid of useless Arc

--- src/ratelimit.rs
+++ src/ratelimit.rs
@@ -1,31 +1,24 @@
-use std::sync::{
-    atomic::{AtomicU64, Ordering},
-    Arc,
-};
+use std::sync::atomic::{AtomicU64, Ordering};
 use tokio::time::{delay_for, Duration};
 
 pub struct RateLimit {
-    counter: Arc<AtomicU64>,
+    counter: AtomicU64,
     limit: u64,
 }
 
 impl RateLimit {
     pub fn new(limit: u64) -> Self {
         Self {
-            counter: Arc::default(),
+            counter: AtomicU64::default(),
             limit,
         }
     }
 
     pub async fn wait(&self) {
-        let limit = self.limit;
-        let counter = Arc::clone(&self.counter);
-        let seconds = counter.fetch_add(limit, Ordering::AcqRel);
-
+        let seconds = self.counter.fetch_add(self.limit, Ordering::AcqRel);
         delay_for(Duration::from_secs(seconds)).await;
-
         if seconds != 0 {
-            counter.fetch_sub(limit, Ordering::AcqRel);
+            self.counter.fetch_sub(self.limit, Ordering::AcqRel);
         }
     }
 }
